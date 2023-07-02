8af69ac739a8f7c7387faa2ae3163a312143df1c
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-02 14:04:12 -0400

    setup ch127

--- section-6/ch127.py
+++ section-6/ch127.py
@@ -1,1 +1,19 @@
 # PRIVATE VS PUBLIC VARIABLES
+
+class PlayerCharacter:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('run')
+
+    def speak(self):
+        print(f'My name is {self.name}, and I am {self.age} years old.')
+
+
+player1 = PlayerCharacter('Daniel', 35)
+
+player1.name = '!!!'
+player1.speak = 'BOOOOO'
