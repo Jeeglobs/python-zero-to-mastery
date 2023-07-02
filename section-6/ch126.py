62195d5a895959d4e0fc95f90092930bc441c667
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-02 10:36:52 -0400

    finish chapter 126

--- section-6/ch126.py
+++ section-6/ch126.py
@@ -13,7 +13,16 @@ class PlayerCharacter:
         print(f'My name is {self.name}, and I am {self.age} years old.')
 
 
+# abstraction -- only see what we need to?
+print((1, 2, 3, 1).count(1))
+
 player1 = PlayerCharacter('Daniel', 35)
 
-# abstraction
-print((1, 2, 3, 1).count(1))
+player1.name = '!!!'
+player1.speak = 'BOOOOO'
+
+# prints BOOOOO
+print(player1.speak)
+
+# throws error b/c .speak() has been changed
+print(player1.speak())
