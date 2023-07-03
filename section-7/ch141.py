9187c331752e2fe6e2bed4db37da47b008758828
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-03 13:41:05 -0400

    finish chapter 141

--- section-7/ch141.py
+++ section-7/ch141.py
@@ -1,1 +1,17 @@
 # MAP()
+
+# def multiply_by2(li):
+#     new_list = []
+#     for item in li:
+#         new_list.append(item*2)
+#     return new_list
+
+
+def multiply_by2(item):
+    return item * 2
+
+
+print(multiply_by2([1, 2, 3]))
+
+print(map(multiply_by2, [1, 2, 3]))
+print(list(map(multiply_by2, [1, 2, 3])))
