cbba4ed3130e8f5513144a8d3ab07c216720f2af
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-03 14:42:29 -0400

    finish chapter 142

--- section-7/ch142.py
+++ section-7/ch142.py
@@ -1,10 +1,18 @@
 # FILTER()
 
+my_list = [1, 2, 3]
+
+
 def multiply_by2(item):
     return item * 2
 
 
-print(multiply_by2([1, 2, 3]))
+def only_odd(item):
+    return item % 2 != 0
 
-print(map(multiply_by2, [1, 2, 3]))
-print(list(map(multiply_by2, [1, 2, 3])))
+
+print(multiply_by2(my_list))
+
+print(list(map(multiply_by2, my_list)))
+
+print(list(filter(only_odd, my_list)))
