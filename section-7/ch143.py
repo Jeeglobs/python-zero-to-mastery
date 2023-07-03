86e22ebc6665ac44cfe2346c12fa2de0f484ed2e
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-03 14:47:53 -0400

    finish chapter 143

--- section-7/ch143.py
+++ section-7/ch143.py
@@ -1,1 +1,23 @@
 # ZIP()
+
+my_list = [1, 2, 3]
+your_list = [10, 20, 30]
+their_list = [5, 4, 3]
+
+
+def multiply_by2(item):
+    return item * 2
+
+
+def only_odd(item):
+    return item % 2 != 0
+
+
+print(multiply_by2(my_list))
+
+print(list(map(multiply_by2, my_list)))
+
+print(list(filter(only_odd, my_list)))
+
+print(list(zip(my_list, your_list)))
+print(list(zip(my_list, your_list, their_list)))
