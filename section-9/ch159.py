096a56b8effc0a4ecef92331c7d4d4cd56e7e639
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-04 16:30:09 -0400

    finish chapter 159

--- section-9/ch159.py
+++ section-9/ch159.py
@@ -1,1 +1,23 @@
 # ERROR HANDLING
+
+# # we want only int but this allows any input
+# age = input('What is your age? ')
+# print(age)
+
+
+# this makes the input an int, but throws error if input is letters
+# age = int(input('What is your age? '))
+# print(age)
+
+
+while True:
+    try:
+        age = int(input('what is your age? '))
+        print(10/age)
+    except ValueError:
+        print('please enter a number')
+    except ZeroDivisionError:
+        print('please enter a number higher than zero')
+    else:
+        print('thank you!')
+        break
