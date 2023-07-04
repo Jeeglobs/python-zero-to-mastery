e9a16969f39af9759aa7b8d25a35637585d6a451
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-04 16:56:45 -0400

    finish chapter 161

--- section-9/ch161.py
+++ section-9/ch161.py
@@ -1,1 +1,15 @@
 # EXERCISES: ERROR HANDLING
+
+while True:
+    try:
+        age = int(input('what is your age? '))
+        print(10/age)
+    except ValueError:
+        print('please enter a number')
+    except ZeroDivisionError:
+        print('please enter a number higher than zero')
+    else:
+        print('thank you!')
+        break
+    finally:
+        print('ok done')
