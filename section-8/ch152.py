decb0d6d7caddb556290c14d8c7bd008db284cd8
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-04 14:53:46 -0400

    finish chapter 152

--- section-8/ch152.py
+++ section-8/ch152.py
@@ -1,1 +1,37 @@
 # DECORATORS
+
+# @classmethod
+# @staticmethod
+
+def hello():
+    print('hello')
+
+
+greet = hello
+
+greet()
+
+del hello
+
+greet()
+
+# print(hello())
+
+'''
+functions can act like variables
+greet() still works after deleting hello
+-> hello is removed from memory but greet still points to the hello function
+'''
+
+
+def goodbye(func):
+    func()
+
+
+def meet():
+    print('nice to meet you!')
+
+
+example = goodbye(meet)
+
+print(example)
