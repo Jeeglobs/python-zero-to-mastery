362b5be06f75410717e158745e2baedaa7bff4b1
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-04 11:11:43 -0400

    start chapter 147

---
+++ section-7/ch147.py
@@ -0,0 +1,16 @@
+# EXERCISE: LAMBDA EXPRESSIONS
+
+'''
+Create a lambda expression that will square my_list
+should print [25, 16, 9]
+'''
+
+my_list = [5, 4, 3]
+
+'''
+List Sorting:
+sort list a by the second value in each tuple
+should print [(10, -1), (0, 2), (4, 3), (9, 9)]
+'''
+
+a = [(0, 2), (4, 3), (9, 9), (10, -1)]
