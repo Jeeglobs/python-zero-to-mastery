1d7a51ff8c74e8c937c9ff057b5e1d948fd82a31
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-05 19:19:40 -0400

    finish chapter 167

--- section-10/ch167.py
+++ section-10/ch167.py
@@ -1,1 +1,35 @@
 # EXERCISE: FIBONACCI NUMBERS
+
+
+# n is F(n) in the fibonacci sequence
+# generator is more memory efficient
+def fib(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        yield a
+        temp = a
+        a = b
+        b = temp + b
+
+
+for x in fib(20):
+    print(x)
+
+
+# using a list here is more taxing on memory
+def fib2(n):
+    a = 0
+    b = 1
+    result = []
+
+    for i in range(n):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    return result
+
+
+print(fib2(30))
