7cc0dbaadba6adff76a2a3ccaeccdab32d03e9bf
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-14 14:42:34 -0400

    finish chapter 182

--- section_11/ch_182.py
+++ section_11/ch_182.py
@@ -1,8 +1,15 @@
 #  USEFUL MODULES 2
 
 import datetime
+from array import array
 
 print(datetime.time())
 print(datetime.time(5, 45, 2))
-
 print(datetime.date.today())
+
+
+# use arrays when lists get very large and you do not want to use generators
+arr = array('i', [1, 2, 3])
+
+print(arr)
+print(arr[0])
