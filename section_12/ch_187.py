1f6187256a0a43c5a8e3f30e96232089f8a89b79
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-15 13:03:34 -0400

    finish chapter 187

--- section_12/ch_187.py
+++ section_12/ch_187.py
@@ -1,1 +1,11 @@
 # FILE PATHS
+
+# use cd .. to go up one level
+# pwd shows you which directory you are currently in
+
+# there are absolute paths and relative paths
+
+# . means current folder
+# .. means back a folder
+
+# pathlib is useful when using machines with different path systems. like windows and mac
