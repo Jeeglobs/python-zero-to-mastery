4ccd6c96881a1c87ce903d1e51a15eea382fbe12
Jeeglobs <dp.hudgins@hotmail.com>
2023-07-29 15:44:09 -0400

    finish chapter 194

--- section_14/ch194.py
+++ section_14/ch194.py
@@ -1,1 +1,10 @@
 # EXERCISE: PASSWORD VALIDATION
+
+'''
+create a password validator
+passords:
+must have at least characters
+can contain letters and/or numbers
+can contain '@', '#', '$', '%'
+BONUS: must end with a number
+'''
