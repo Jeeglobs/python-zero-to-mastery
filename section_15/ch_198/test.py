8ffc289ad64dabbd9a0f2bc2cc351933d628c56f
Jeeglobs <dp.hudgins@hotmail.com>
2023-08-01 15:45:28 -0400

    finish chapter 198

--- section_15/ch_198/test.py
+++ section_15/ch_198/test.py
@@ -12,7 +12,17 @@ class TestMain(unittest.TestCase):
     def test_do_stuff_2(self):
         test_param = 'string'
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff_3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter a number')
+
+    def test_do_stuff_4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter a number')
 
 
 unittest.main()
